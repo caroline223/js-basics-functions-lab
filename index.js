c2d982aa9b46c1c491834f08b98970b170e3e234
caroline223 <carolineforrester22@gmail.com>
2021-03-18 09:53:26 -0400

    commit tree bbd8f52f829f44157bbc833b75feaf67ba06a1f1

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(street){
+    if (street >= 42){
+        return street - 42;
+    } else {
+        return 42 - street;
+    }
+}
+
+function distanceFromHqInFeet(street){
+    return distanceFromHqInBlocks(street) * 264;
+}
+
+function distanceTravelledInFeet(start, end){
+    if (start > end ){
+        return (start - end) * 264;
+    } else {
+        return (end - start) * 264;
+    }
+}
+
+function calculatesFarePrice(start, end){
+    if (distanceTravelledInFeet(start, end) < 400) {
+        return 0
+    } else if ((distanceTravelledInFeet(start, end)) >= 400 && (distanceTravelledInFeet(start, end)) <= 2000){
+        return (distanceTravelledInFeet(start, end) - 400) * .02 
+    } else if ((distanceTravelledInFeet(start, end)) > 2000 && (distanceTravelledInFeet(start, end)) <= 2500 ) {
+        return 25
+    } else if (distanceTravelledInFeet(start, end) > 2500) {
+        return 'cannot travel that far'
+    }
+
+}
